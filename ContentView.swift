f7a3477f75230ad17e6363b0353e170d929a8e06
Brandon Naude <naudebrandon@gmail.com>
2024-07-01 08:45:58 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.green)
+            Text("Hello, world!")
+        }
+    }
+}
